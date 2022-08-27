b6b8141fde048ce756aa8773277f1ec4814c5d8b
Hunter Kelley <hkelley534@gmail.com>
2022-08-27 00:52:50 -0400

    commit 4

---
+++ jennifer-nguy-react/jennifer-nguy/src/Introduction.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Styles/home.css'
+import lotus from './assets/lotus.png'
+
+function Introduction() {
+    return(
+        <div id="introduction">
+            {/*  Intro Text  */}
+            <div id="intro-text">
+                {/*  Intro Title  */}
+                <div id="intro-title">
+                    <h1>
+                        Jennifer Nguy
+                    </h1>
+                </div>
+                {/*  Intro Description  */}
+                <div id="intro-description">
+                    <h1>VISUAL ARTIST</h1>
+                    <h1>+</h1>
+                    <h1>DESIGNER</h1>
+                </div>
+                {/*  About Link  */}
+            </div>
+            {/*  Intro Image  */}
+            <div id="intro-img">
+                <img id="lotus-img" src={lotus} alt="lotus" />
+            </div>
+            <div id='button-container'>
+                <button class="about-button">
+                    <a href="About.html"><b>About Me</b></a>
+                </button>
+            </div>
+        </div>
+    )
+};
+
+export default Introduction;
